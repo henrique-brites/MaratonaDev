a819e1e87e0a67276935171c8f9592b0dd94546b
Henrique Brites <henriquebrites@live.com>
2020-02-25 12:21:31 -0300

    Configurando o arquivo server.js para fazer o cadastro e a leitura do Banco de Dados MySQL

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const express = require("express");
 const server = express();
 
 // configurar o servidor para apresentar arquivos estáticos
-server.use(express.static('public'));
+server.use(express.static('./public'));
 
 // habilitar body do formulario
-server.use(express.urlencoded({ extended: true }));
+server.use(express.urlencoded({ 
+  extended: true 
+}));
+
+// habilitando a conexão com o banco de dados
+const mysql = require('mysql2');
+const db = mysql.createPool({
+  host     : 'localhost',
+  user     : 'usrstore1',
+  password : 'ncs1st3m4s!@',
+  database : 'doe',
+  debug    :  false
+});
 
 // configurando a tamplate engine
 const nunjucks = require("nunjucks");
@@ -15,49 +27,47 @@ nunjucks.configure("./", {
   noCache: true,
 });
 
-
-// lista de doadores: Vetor ou Array
-const donors = [
-  {
-    name: "Henrique Brites",
-    blood: "AB+"
-  },
-  {
-    name: "Luiz Flavio",
-    blood: "B+"
-  },
-  {
-    name: "Pedro Silva",
-    blood: "A+"
-  }
-  ,{
-    name: "Maria Brites",
-    blood: "O+"
-  }
-]
-
-
-
-
 // configurar a apresentação da página
-server.get("/", function(req, res) {
-  return res.render("index.html", { donors });
+server.get("/", function (req, res) {
+
+  const query = `SELECT * FROM donors`;
+
+  db.query(query, function (err, result) {
+      if (err)
+          return res.send("Erro de banco de dados!");
+
+      const donors = result;
+
+      return res.render("index.html", {
+          donors
+      });
+  });
 });
 
-server.post("/", function(req, res) {
+server.post("/", function (req, res) {
+  //pegar dados do formulário
   const name = req.body.name;
   const email = req.body.email;
   const blood = req.body.blood;
 
-  donors.push({
-    name: name,
-    blood: blood,
+  if (name == "" || email == "" || blood == "") {
+      return res.send("Todos os campos são obrigatórios!")
+  }
+  // coloco os valores dentro do banco de dados.
+  const query = `INSERT INTO donors (name, email, blood) VALUES (?, ?, ?)`;
+
+  const values = [name, email, blood];
+
+  db.query(query, values, function (err) {
+      if (err)
+          return res.send("Erro cadastro no banco de dados!");
+
+      return res.redirect("/");
   });
 
-  return res.redirect("/");
 });
 
 // ligar o servidor e permitir o acesso na portaq 3000
 server.listen(3000, function() {
-  console.log("iniciei o servidor");
+  console.log("Iniciei o servidor!");
 });
